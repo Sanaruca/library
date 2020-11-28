e5b28489e46c9f3e1438e6c8cb5ee6fb989659da
Sanaruca <68965395+Sanaruca@users.noreply.github.com>
2020-11-27 23:44:28 -0400

    Archivos añadidos

---
+++ scripts/classes/book.js
@@ -0,0 +1,8 @@
+class Book{
+  constructor(title,author,description,pages,read = false){
+    this.title = title;
+    this.description = description;
+    this.pages = pages;
+    this.read = read;
+  }
+}
